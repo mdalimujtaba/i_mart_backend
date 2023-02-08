656b6584f263e1319fe7323f66f16cac2ae3ce37
mdalimujtaba <mdalimujtaba29850@gmail.com>
2023-02-08 20:23:30 +0530

    edited

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { authentication } = require("../Authentication/CartAuthentication");
+const { authentication } = require("../Authentication/cartAuthentication");
 const { CartModel } = require("../model/cartModel");
 const cartRoute = express.Router();
 
