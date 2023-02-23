6358f06b448c6b5e96960dc365576ac300c64028
mdalimujtaba <mdalimujtaba29850@gmail.com>
2023-02-23 19:31:54 +0530

    update order route

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,5 +1,5 @@
 const express=require("express")
-const { authentication } = require("../Authentication/CartAuthentication")
+const { authentication } = require("../Authentication/cartAuthentication")
 const { orderModel } = require("../model/placeorderModel")
 const orderRoute=express.Router()
 
